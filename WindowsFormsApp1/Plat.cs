bd657a8ee22b05da2e86a6e2d3c0c92b2066a945
Samuel <samuel.chatelet@gmail.com>
2018-04-16 16:32:54 +0200

    Ajout form + classe Plat

---
+++ WindowsFormsApp1/Plat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Plat
+    {
+        private string nom;
+        private double prix;
+        private int quantite;
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public double Prix
+        {
+            get { return prix; }
+            set { prix = value; }
+        }
+
+        public int Quantite
+        {
+            get { return quantite; }
+            set { quantite = value; }
+        }
+
+
+        public Plat() { }
+        public Plat(string no, double px, int qt)
+        {
+            nom = no;
+            prix = px;
+            quantite = qt;
+        }
+        public override string ToString()
+        {
+            return "\r\nNom: " + nom + "\r\nPrix: " + prix + "\r\nQuantité: " + quantite;
+        }
+    }
+}
