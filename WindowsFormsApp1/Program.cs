20132f50e6fc3c498eb905edd6bed472d82f2bab
Samuel <samuel.chatelet@gmail.com>
2018-04-17 14:29:01 +0200

    Oublie

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -12,6 +12,21 @@ namespace WindowsFormsApp1
         /// Point d'entrée principal de l'application.
         /// </summary>
         [STAThread]
+
+        static int Rechercher(List<Plat> L, String plat)             //fonction qui permet de vérifier l'existence d'un plat
+        {
+            int p = -1;
+            for (int i = 0; i < L.Count; i++)
+            {
+                if (L[i].Nom == plat)
+                {
+                    p = i;
+                    break;
+                }
+            }
+            return p;
+        }
+
         static void Main()
         {
             Application.EnableVisualStyles();
